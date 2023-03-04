56ada664ae5cf7258f8b73c430674ca67d4cc4ba
ArnMak <arntengu@gmail.com>
2023-03-04 18:09:03 +0200

    first section

--- gulpfile.js
+++ gulpfile.js
@@ -11,13 +11,11 @@ gulp.task('server', function() {
         server: {
             baseDir: "src"
         }
-    });
-
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+    });    
 });
 
 gulp.task('styles', function() {
-    return gulp.src("src/scss/**/*.+(scss|sass)")
+    return gulp.src("src/sass/**/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({suffix: '.min', prefix: ''}))
         .pipe(autoprefixer())
@@ -28,6 +26,7 @@ gulp.task('styles', function() {
 
 gulp.task('watch', function() {
     gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
-})
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+});
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
