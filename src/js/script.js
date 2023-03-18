24f65fa280b819a201fba008660fd26b21e35384
ArnMak <arntengu@gmail.com>
2023-03-18 12:03:30 +0200

    add mailer and animations

--- src/js/script.js
+++ src/js/script.js
@@ -79,4 +79,44 @@ $(document).ready(function(){
     valideForms('#order form');
 
     $('input[name=phone]').mask("+38 (999) 999-99-99");
+
+    $('form').submit(function(e){
+        e.preventDefault();
+
+        if (!$(this).valid()){
+            return;
+        };
+
+        $.ajax({
+            type:"POST",
+            url:"mailer/smart.php",
+            data:$(this).serialize()
+        }).done(function(){
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+    //Плавный скрол и pageup
+
+    $(window).scroll(function(){
+        if($(this).scrollTop()>1600){
+            $('.pageup').fadeIn();
+        } else{
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href^='#']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+    new WOW().init();
+
   });
