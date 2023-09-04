2d690f38360a8e8fb3b2db9b2a276f880d2e88d1
Artem Makarov <arntengu@gmail.com>
2023-09-04 09:45:02 +0300

    mailer change

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -13,13 +13,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'arntengu@gmail.com';                 // Наш логин
-$mail->Password = 'wqvkjpvhziloawiz';                           // Наш пароль от ящика
+$mail->Username = 'example@gmail.com';                 // Наш логин
+$mail->Password = 'wqvcjpvhziloawiz';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('arntengu@gmail.com', 'Pulse');   // От кого письмо 
-$mail->addAddress('makarovarn07@gmail.com');     // Add a recipient
+$mail->setFrom('example@gmail.com', 'Pulse');   // От кого письмо 
+$mail->addAddress('yours@gmail.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
